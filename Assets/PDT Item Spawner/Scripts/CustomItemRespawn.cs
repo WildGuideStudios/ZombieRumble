586f5141ccc810b62adda5cbd6677741c9023432
Eric Niconovich <7265626F6F74@users.noreply.github.com>
2018-05-31 19:38:10 -0400

    Initial Upload
    This is the first upload of the current game files.

---
+++ Assets/PDT Item Spawner/Scripts/CustomItemRespawn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomItemRespawn : MonoBehaviour {
+
+    [Header("Delay in Seconds")]
+    public float delay = 0;
+    private float counter = 0;
+
+    CustomItemSpawner spawner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        try
+        {
+            spawner = GetComponent<CustomItemSpawner>();
+        }
+        catch { }
+		
+        if(transform.childCount == 0)
+        {
+
+            if ((counter += Time.deltaTime) >= delay)
+            {
+                counter = 0;
+
+                try
+                {
+
+                    
+                   GetComponent<CustomItemSpawner>().spawned = false;
+                   GetComponent<CustomItemSpawner>().triggered = false;
+                    
+
+                }
+                catch
+                {
+
+                }
+            }
+
+        }
+
+
+	}
+}
---
+++ Assets/Scripts/KillEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemy : MonoBehaviour
+{
+    void Update()
+    {
+
+    try {
+            if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
+        }
+
+        catch (System.NullReferenceException)
+        {
+            Debug.Log ("Don't forget to add EventSystem object in hirarchy");
+        }
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Zombie")
+            {
+                Debug.Log("Zombie Hit!");
+            }
+            else
+            {
+                Debug.Log("You Missed!");
+            }
+        }
+    }
+}
