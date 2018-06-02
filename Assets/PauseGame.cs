e1e815b4cd921426ad2ff7f0ee4dd569fb0e8a16
c0nques7 <c0nques7@gmail.com>
2018-06-02 16:38:07 -0400

    menu items
    menu has been created, utilizes button events and gamecontroller object

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseGame : MonoBehaviour {
+    public Transform canvas;
+    public Transform hudCanvas;
+    public Transform Player;
+    public Transform MenuCamera;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+
+    }
+    public void Pause()
+    {
+            if (canvas.gameObject.activeInHierarchy == false && hudCanvas.gameObject.activeInHierarchy == true)
+        {
+
+            canvas.gameObject.SetActive(true);
+            hudCanvas.gameObject.SetActive(false);
+            Time.timeScale = 0;
+            Player.GetComponent<FirstPersonController>().enabled = false;
+        }
+
+        else if (MenuCamera.gameObject.activeInHierarchy == true)
+        {
+            Menu();
+        }
+
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            hudCanvas.gameObject.SetActive(true);
+            Time.timeScale = 1;
+            Player.GetComponent<FirstPersonController>().enabled = true;
+        } 
+            
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1;
+    }
+    
+}
