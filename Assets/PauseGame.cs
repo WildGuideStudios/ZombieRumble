99b0a121acf0905005de40cf3ddccc7ee78e55a4
7265626F6F74 <7265626F6F74@users.noreply.github.com>
2018-06-06 19:33:45 -0400

    some logging for troubleshooting

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -14,43 +14,36 @@ public class PauseGame : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape) && Player.gameObject.activeInHierarchy == true)
-        {
+        if (Input.GetKeyDown(KeyCode.Escape) && Player.gameObject.activeInHierarchy == true) {
             Pause();
         }
-
     }
+
     public void Pause()
     {
-            if (canvas.gameObject.activeInHierarchy == false && hudCanvas.gameObject.activeInHierarchy == true)
-        {
-
+        string sname = "PauseGame.cs - ";
+        if (canvas.gameObject.activeInHierarchy == false && hudCanvas.gameObject.activeInHierarchy == true) {
+            Debug.Log(sname + "Paus function if");
             canvas.gameObject.SetActive(true);
             hudCanvas.gameObject.SetActive(false);
             Time.timeScale = 0;
             Player.GetComponent<FirstPersonController>().enabled = false;
-        }
-
-        else if (Player.gameObject.activeInHierarchy == false)
-        {
+        } else if (Player.gameObject.activeInHierarchy == false) {
+            Debug.Log(sname + "Paus function else if");
             Menu();
-        }
-        else
-        {
+        } else {
+            Debug.Log(sname + "Paus function else");
             canvas.gameObject.SetActive(false);
             hudCanvas.gameObject.SetActive(true);
             Time.timeScale = 1;
             Player.GetComponent<FirstPersonController>().enabled = true;
         } 
-            
     }
 
-    public void Menu()
-    {
+    public void Menu() {
         Time.timeScale = 1;
         MenuCamera.gameObject.SetActive(true);
         ZombieSpawn.gameObject.SetActive(false);
         ObjectSpawn.gameObject.SetActive(false);
     }
-    
 }
